b9d7e83f1601cef82acb36e1d60ba43daa79eece
astha <asthaavasthy@gmail.com>
2024-01-14 13:13:37 +0530

    commit changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,9 @@
+import { useState } from "react";
 import { LOGO_URL } from "../Utils/constants";
 const Header = () =>
-(
-   
-    (
+{
+   const [btnNameReact,setBtnNameReact] = useState("Login");
+   return (
         <div className="header">
             <div className="logo-container">
                 <img className="logo"
@@ -14,10 +15,15 @@ const Header = () =>
                     <li>About Us</li>
                     <li>Contact Us</li>
                     <li>Cart</li>
+                    <button className="login" onClick={() =>
+                    {
+                    btnNameReact === "Login"  ? setBtnNameReact("Logout")
+                    : setBtnNameReact("Login");
+                    }}>{btnNameReact}</button>
                 </ul>
             </div>
         </div>
     )
-)
+   }
 
 export default Header;
